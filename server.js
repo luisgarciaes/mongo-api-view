a4c39cfacb1b4c8cd8d6fb28bbbe561665552066
visita <visita@TELEMATICA.UCOL.MX>
2022-05-25 08:08:15 -0500

    added main view and route

--- server.js
+++ server.js
@@ -1,14 +1,18 @@
 let express = require('express');
 const mongoose = require('mongoose')
-const app =  express()
+const app = express()
 
-let personsRoute = require('./routes/person');
+let personRoute = require('./routes/person');
+let personsRoute = require('./routes/persons');
+let mainRoute = require('./routes/main');
 
 mongoose.Promise = global.Promise;
 
 app.set('view engine', 'ejs');
-app.use(express.urlencoded({extended:false}))
+app.use(express.urlencoded({ extended: false }))
+app.use(personRoute);
 app.use(personsRoute);
+app.use(mainRoute);
 app.use('/assets', express.static(__dirname + '/public'));
 
 
@@ -16,12 +20,12 @@ let PORT = process.env.PORT || 3000;
 
 require("dotenv").config();
 mongoose.connect(process.env.MONGODB_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-  .then((db) => console.log("Mongodb is connected"))
-  .catch((err) => console.error(err));
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    })
+    .then((db) => console.log("Mongodb is connected"))
+    .catch((err) => console.error(err));
 
-app.listen(PORT,()=>{
+app.listen(PORT, () => {
     console.log('escuchando en el puerto 3000')
 })
